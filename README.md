b210dcc297a95a3cc763c82630711a35d84a9ca3
IamXGW <iamxgw@gmail.com>
2021-06-23 13:47:40 +0800

    P23 Nacos 配置中心简单示例正确运行

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gulimall-IamXGW
+
 《谷粒商城》项目学习代码仓。
+
+实现过程中遇到的错误我都发布在了个人网站[《谷粒商城》项目错误合集macOS](https://xuguangwei.com/posts/264117559/)
---
+++ gulimall-member/src/main/java/com/iamxgw/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,12 @@
+package com.iamxgw.gulimall.member.feign;
+
+import com.iamxgw.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
